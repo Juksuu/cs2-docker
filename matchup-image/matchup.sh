fb31f69e00c116c23564eab325806c9867446293
Frans Paasonen <franspaasonen@gmail.com>
2024-02-11 18:48:15 +0200

    feat: Update matchup plugin if MATCHUP_VERSION env is changed

--- matchup-image/matchup.sh
+++ matchup-image/matchup.sh
@@ -6,16 +6,28 @@ base_folder=${HOME}/${STEAMAPPDIR}/game/csgo
 addons_folder=$base_folder/addons
 gameinfo=$base_folder/gameinfo.gi
 
-matchup_download_url="https://github.com/Juksuu/MatchUp/releases/download/${MATHCUP_VERSION-"v0.1.0"}/MatchUp+cssharp+metamod.zip"
+matchup_version=${MATCHUP_VERSION-"v0.1.0"}
+matchup_version_file=$addons_folder/matchup_version_$matchup_version
+matchup_download_url="https://github.com/Juksuu/MatchUp/releases/download/$matchup_version/MatchUp+cssharp+metamod.zip"
 
 gameinfo_string_match='\t\t\tGame_LowViolence\tcsgo_lv // Perfect World content override'
 gameinfo_insert_line='\t\t\tGame\tcsgo/addons/metamod'
 
 install_matchup() {
+  install=false
   if [ ! -d "$addons_folder" ]; then
+    install=true
+  elif [ ! -f "$matchup_version_file" ]; then
+    install=true
+    rm -rf $addons_folder
+  fi
+
+  if [ $install = true ]; then
     echo "installing matchup"
     cd $base_folder
     wget -qO- $matchup_download_url | busybox unzip -
+
+    touch $matchup_version_file
   fi
 }
 
